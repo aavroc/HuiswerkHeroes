8cd5c1077bf0122ea3cca4fa40aa3eb1d21993af
tunt67 <57668576+tunt67@users.noreply.github.com>
2019-11-15 00:31:28 +0100

    OOP voor de studenten login register ETC.
    werkt nog niet om een of andere reden herkent de database de igevulde waardes niet die overeenkomen met het formulier van de inlog file. Misschien moeten we een nieuwe database aanmaken ik heb het gevoel dat er iets niet klopt met deze database

---
+++ studentclass.php
@@ -0,0 +1,53 @@
+<?php 
+class User
+{
+	protected $db;
+	protected $message = '';
+	
+	//construct voor database inloggegevens variabelen
+	public function __construct()
+	{
+		$user = 'root';
+		$pass = '';
+		$this->db = new PDO('mysql:host=localhost;dbname=huiswerkheroes', $user, $pass);
+
+    }
+    public function getMessage()
+    {
+        return $this->message;
+    }
+    public function login($email_form, $password_form)
+    {
+        //query die de gegevens na leest
+        $sql = "SELECT * FROM pdo WHERE 'username' = :username AND 'password' = :pass";
+        $statement = $this->db->prepare($sql);
+        $statement->bindParam(":username", $email_form);
+        $statement->bindParam(":pass", $password_form);
+        $statement->execute();
+        $database_gegevens = $statement->fetch(PDO::FETCH_ASSOC);
+
+        
+
+        //checked of databasegegevens een array is maar ook of het gevult is met data
+        if(is_array($database_gegevens) && !empty($database_gegevens)){
+            $this->message = 'Gebruiker bestaat';
+            //check of het ingevulde wachtwoord gelijk is aan dat van het wachtwoord van de gebruikers
+            if($database_gegevens['password'] == $password_form){
+                $this->message =  'De gebruiker is succesvol ingelogd';
+                //ingelogd
+                session_start();
+                
+                $_SESSION["user"] = $database_gegevens['id'];
+                $_SESSION["email"] = $database_gegevens['username'];
+                $_SESSION["status"] = TRUE;
+                
+                //stuurt de gebruiker door naar de stellingen pagina
+                header( "Location: dashboard.php" );
+            }
+        }
+        else{
+            echo "werkt niet";
+        }
+    }
+}
+?>
