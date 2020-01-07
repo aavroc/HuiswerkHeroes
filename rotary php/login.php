8ca311c122c35e22387c99e2b338a68c476bc2f7
Mink <2078927@talnet.nl>
2020-01-07 16:13:30 +0100

    paar dinge aan veranderd (clean code)

--- rotary php/login.php
+++ rotary php/login.php
@@ -5,6 +5,7 @@ require 'config.php';
 if (isset($_POST['login'])) {
 	$errMsg = '';
 
+<<<<<<< HEAD
 	//Alles wat in de post gestopt wordt (wat je in de forms invult) wordt in de var gestopt
 	$username = $_POST['username'];
 	$password = $_POST['password'];
@@ -47,11 +48,75 @@ if (isset($_POST['login'])) {
 		} catch (PDOException $e) {
 			echo $e->getMessage();
 		}
+=======
+		// Get data from FORM
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+
+		// check if fields are set
+		if(empty($username) || empty($password))
+		{
+			$errMsg = 'Alle velden moeten ingevuld worden.';
+		} 
+		else
+		{
+			try
+			{
+				// SQL query for login. Also grabs data that will later be used into sessions.
+				$sql = "SELECT * FROM user WHERE username = :gebruikersnaam";
+				$statement = $connect->prepare($sql);
+				$statement->bindParam(":gebruikersnaam", $username);
+				$statement->execute();
+				
+				$database_gegevens = $statement->fetch(PDO::FETCH_ASSOC); 
+				if($database_gegevens == FALSE)
+				{
+					header("location: login.php?error=dbconnFailed");
+					exit();
+				
+				}
+				else
+				{
+					// password and hashed password check if it's the same.
+					$passcheck = password_verify($password, $database_gegevens['password']);
+					if($passcheck == FALSE)
+					{
+						header("location: login.php?error=invalidLogin");
+						exit();
+					}
+					else if($passcheck == TRUE && $username = $database_gegevens['username'])
+					{
+						$_SESSION['fullname'] = $database_gegevens['fullname'];
+						$_SESSION['username'] = $database_gegevens['username'];
+						$_SESSION['niveau'] = $database_gegevens['niveau'];
+						$_SESSION['id'] = $database_gegevens['id'];
+
+						header("location: dashboard.php?login=succes");
+						exit();
+					}
+					else
+					{
+						
+						header("location: login.php?error=invalidData");
+						exit();
+					}
+					
+				}
+			}
+			catch(PDOException $e)
+			{
+				echo $e->getMessage();
+			}
+		}
+		
+>>>>>>> f916736240229d922ea969ebb99f15858b156f2c
 	}
 }
 ?>
 
 <html>
+<<<<<<< HEAD
 
 <head>
 	<title>Login</title>
@@ -59,6 +124,17 @@ if (isset($_POST['login'])) {
 <style>
 	html,
 	body {
+=======
+<head><title>Login</title></head>
+	<body>
+	<form method="post">
+              email: <input type="email" name="email" onfocus="this.value=''" value="email"><br>
+              password: <input type="password" name="password" onfocus="this.value=''" value="password"><br>
+            <input type="submit" name="login" class="btn btn-success mt-2" value="login">
+        </form>
+	<!-- <style>
+	html, body {
+>>>>>>> f916736240229d922ea969ebb99f15858b156f2c
 		margin: 1px;
 		border: 0;
 	}
@@ -67,6 +143,7 @@ if (isset($_POST['login'])) {
 <body>
 	<div align="center">
 		<div style=" border: solid 1px #006D9C; " align="left">
+<<<<<<< HEAD
 			<?php
 			if (isset($errMsg)) {
 				echo '<div style="color:#FF0000;text-align:center;font-size:17px;">' . $errMsg . '</div>';
@@ -78,10 +155,29 @@ if (isset($_POST['login'])) {
 					<input type="text" name="username" placeholder="Gebruikersnaam" value="<?php if (isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box" /><br /><br />
 					<input type="password" name="password" placeholder="Wachtwoord" value="<?php if (isset($_POST['password'])) echo $_POST['password'] ?>" autocomplete="off" class="box" /><br /><br />
 					<input type="submit" name='login' value="Login" class='submit' /><br />
+=======
+			
+			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Login</b></div>
+			<div style="margin: 15px">
+				<form action="" method="post">
+<<<<<<< HEAD
+					<input type="text" name="username" value="username" autocomplete="off" class="box"/><br /><br />
+					<input type="password" name="password" value="password" autocomplete="off" class="box" /><br/><br />
+=======
+					<input type="text" name="username" placeholder="Gebruikersnaam" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box"/><br /><br />
+					<input type="password" name="password" placeholder="Wachtwoord" value="<?php if(isset($_POST['password'])) echo $_POST['password'] ?>" autocomplete="off" class="box" /><br/><br />
+>>>>>>> b9365b4c47215e5326dfcf96a76da8c19dd5fc88
+					<input type="submit" name='login' value="Login" class='submit'/><br />
+>>>>>>> f916736240229d922ea969ebb99f15858b156f2c
 				</form>
 			</div>
 		</div>
-	</div>
+	</div> -->
 </body>
+<<<<<<< HEAD
 
 </html>
+=======
+</html>
+<?php echo $user->getMessage();?>
+>>>>>>> f916736240229d922ea969ebb99f15858b156f2c
--- rotary php/logout.php
+++ rotary php/logout.php
@@ -1,5 +1,15 @@
+<<<<<<< HEAD
 <?php
 	require 'config.php';
 	session_destroy();
 	header('Location: index.php');
 	exit(); //je wordt uitgelogt en naar index.php gestuurd
+=======
+
+<?php
+	require 'config.php';
+	session_destroy();
+
+	header('Location:../index.php');
+?>
+>>>>>>> f916736240229d922ea969ebb99f15858b156f2c
