499c027a0ecaf80e8624875ffddd49f6d50bba15
Alikurnaz70 <45204288+Alikurnaz70@users.noreply.github.com>
2020-01-28 17:20:59 +0100

    .

--- db/Util/util.php
+++ db/Util/util.php
@@ -2,7 +2,7 @@
 $user = "root";
 $pass = "";
 $host = "localhost";
-$dbname = "";
+$dbname = "rotary_huiswerkheroes";
 $sql_file = fopen('RHWH_28-01-20.sql', 'r');
 $sql = fread($sql_file, filesize('RHWH_28-01-20.sql'));
 $conn = new PDO("mysql:host=" . $host . ";dbname=" . $dbname , $user, $pass);
